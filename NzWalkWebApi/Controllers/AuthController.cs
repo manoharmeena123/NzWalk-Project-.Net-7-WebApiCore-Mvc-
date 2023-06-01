70a21237fe12024f6c2b8f502a48364c68856b72
user <manoharmeena245@gmail.com>
2023-06-01 12:02:18 +0530

    versioning

--- NzWalkWebApi/Controllers/AuthController.cs
+++ NzWalkWebApi/Controllers/AuthController.cs
@@ -21,6 +21,8 @@ namespace NzWalkWebApi.Controllers
         }
 
 
+
+
         //POST: Register==================================================================>
         [HttpPost]
         [Route("Register")]
